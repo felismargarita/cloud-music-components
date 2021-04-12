93ead9d8d44902863528b9564a21ed9f145fe237
felis <felismargarita@hotmail.com>
2021-04-12 09:26:22 +0800

    pageheader className前缀修改

--- src/PageHeader/index.tsx
+++ src/PageHeader/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Icon from '../Icon/index'
+import {classNamePrefix} from '@/const/index'
 interface HeaderProps {
   content:string
   onClick?:()=>void
@@ -12,10 +13,10 @@ const Header:React.FC<HeaderProps> = ({content,onClick,icon})=>{
 
   return (
     <div 
-    className="cloud-music-page-header"
+    className={`${classNamePrefix}-page-header`}
     onClick={()=>onClick?.()} 
     >
-      {content}{<span className="cloud-music-page-header-icon">{pageIcon}</span>}
+      {content}{<span className={`${classNamePrefix}-page-header-icon`}>{pageIcon}</span>}
     </div>
   )
 }
